9d4aaa0fcaca3f391cd226e463b41c393a7fc0b3
lslinnet <larslinnet@gmail.com>
2014-04-25 11:39:56 +0200

    reverted back into running it as root instead of vagrant

--- modules/drush/manifests/init.pp
+++ modules/drush/manifests/init.pp
@@ -2,13 +2,11 @@ class drush () {
   exec {"install-drush":
     command => "/usr/local/bin/composer global require drush/drush:6.*",
     creates => "/home/vagrant/.composer/vendor/drush",
-    user => 'vagrant',
   }
 
   exec {"add-composer-to-bashrc":
     path => '/bin/',
     command => 'echo \'export PATH="/home/vagrant/.composer/vendor/bin:$PATH"\' >> /home/vagrant/.bashrc',
     unless => 'grep -q \'composer\' /home/vagrant/.bashrc',
-    user => 'vagrant',
   }
 }
